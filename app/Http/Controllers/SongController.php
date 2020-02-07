3ba371ecbbb9d5f2f3e73d1dd74eff2ba5ddea34
alexandre-gerault <gerault-alexandre@orange.fr>
2020-02-07 16:12:53 +0100

    refactor: has player middleware

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Player;
 use App\Song;
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -14,16 +15,10 @@ class SongController extends Controller
     /**
      * Store a song
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function store()
     {
-        if (auth()->guest() || auth()->check() && ! auth()->user()->player instanceof Player) {
-            return response()->json([
-                'message' => "You have to be logged and be related to a player!"
-            ], 403);
-        }
-
         $song = Song::create([
             'player_id' => auth()->user()->player->id,
             'added_by_id' => auth()->user()->id
